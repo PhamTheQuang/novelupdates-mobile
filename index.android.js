0bbc85433be2ad8b6e7528b260693bae009b904e
QuangP+ThaoH <quang.pham+thao.hoang@eastagile.com>
2017-01-06 17:29:46 +0700

    Initialize commit

---
+++ index.android.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+
+import App from './src/App'
+
+export default class novelupdates extends Component {
+  render() {
+    <App />
+  }
+}
+
+AppRegistry.registerComponent('novelupdates', () => novelupdates);
